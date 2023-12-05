4a403e883a7e4a6af75cae7323899d6dee7b0197
sangameshgirne <116374884+sangameshgirne@users.noreply.github.com>
2023-12-05 18:11:25 +0530

    Create pipo.v

---
+++ PIPO/pipo.v
@@ -0,0 +1,18 @@
+module pipov(clk,rst,load,in,out);
+input clk,rst,load;
+input [3:0]in;
+output [3:0]out;
+reg [3:0]temp;
+always@(posedge clk)
+begin
+if(rst)
+temp<=4'b0000;
+else if(load)
+temp<=in;
+else begin
+temp<={0,temp[3:1]};
+end
+end
+
+assign out=temp;
+endmodule
